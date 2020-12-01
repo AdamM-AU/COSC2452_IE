6e99a6b5c399898f86efddc295ef6a9836f404b7
AdamM-AU <70795583+AdamM-AU@users.noreply.github.com>
2020-12-01 12:20:19 +1100

    Clean up

--- Week-01/Week01-Template/src/PleaseRenameMe.java
+++
@@ -1,9 +0,0 @@
-public class PleaseRenameMe {
-	public static void main(String[] args) {
-		GTerm gt = new GTerm(600, 400);
-		gt.print("Hello ");
-		String userInput = gt.getInputString("Who are you?");
-		gt.println(userInput + "!");
-		gt.showHelp();
-	}
-}
